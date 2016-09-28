58b9cd7da54585d6cb84b3a9d31f256cc988d56b
Geoffroy Couprie <geo.couprie@gmail.com>
2016-09-28 15:04:54 +0200

    Start rewriting flow_job_execute

--- imageflow_core/src/parsing/mod.rs
+++ imageflow_core/src/parsing/mod.rs
@@ -6,6 +6,7 @@ use std;
 extern crate rustc_serialize;
 extern crate libc;
 
+use flow;
 use ContextPtr;
 use JsonResponse;
 use libc::c_void;
@@ -180,7 +181,7 @@ impl BuildRequestHandler {
             }
 
 
-            if !::ffi::flow_job_execute(p, job, &mut g) {
+            if !flow::job_execute(p, job, &mut g) {
                 ctx.assert_ok(Some(g));
             }
 
